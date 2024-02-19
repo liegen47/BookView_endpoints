dc45f66f45f88fb1f72ba615ab4a564e9d4498ff
liegen47 <vanshkapoor418@gmail.com>
2024-02-20 02:32:39 +0530

    first commit

---
+++ router/auth_users.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [{"username":"Vansh","password":"password"}];
+
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
+}
+
+const authenticatedUser = (username,password)=>{ 
+    let matchedusers=users.filter((user)=>user.username===username && user.password===password)
+return matchedusers.length > 0;
+}
+
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+    const username=req.body.username;
+    const password=req.body.password;
+
+    if(!username || !password){
+        return res.status(404).json({message:"Error logging in!"})
+    }
+
+    if (authenticatedUser(username,password)){
+        let accessToken=jwt.sign({
+            data:password
+        },'access',{expiresIn: 60*60});
+
+        req.session.authorization={
+            accessToken,username
+        }
+        return res.status(200).send("User successfully logged in")
+    }else{
+        return res.status(404).json({message:"Invalid Login. Check username and password"})
+    }
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+ const ISBN=req.params.isbn;
+ const review=req.body.review;
+ const username=req.session.authorization.username;
+
+ if(books[ISBN]){
+    let book=books[ISBN];
+    book.reviews[username]=review;
+    return res.status(200).send("Review successfully posted")
+ }else{
+    return res.status(404).json({message:"ISBN ${isbn} not found"})
+ }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const ISBN=req.params.isbn;
+    const username=req.session.authorization.username;
+   
+    if(books[ISBN]){
+       let book=books[ISBN];
+       delete book.reviews[username]
+       return res.status(200).send("Review successfully deleted!")
+    }else{
+       return res.status(404).json({message:"ISBN ${isbn} not found"})
+    }
+   });
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
